70f811a07ef9bbf5b7fc87eb6b4a3a0c8fdc687b
darrylbalderas <drrylbalderas@gmail.com>
2020-11-15 05:26:37 -0800

    Create initial django graphql api

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # be-modern
+
+## Instructions
+
+```bash
+python3 -m venv graphdjango
+source graphdjango/bin/activate
+pip install -r requirements.txt
+django-admin startproject modern_catalog .
+django-admin startapp programs
+python manage.py makemigrations
+python manage.py migrate
+python manage.py loaddata programs
+```
+
+## Questions
+
+- Can activites be used in multiple sections ?
+- Is there any image extension we are limited ?
+- Is your postgres hosted on AWS ? Aurora ?
---
+++ modern_catalog/programs/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+# from enum import IntEnum
+
+# Create your models here.
+# class ActivityChoices(IntEnum):
+#     CUSTOMER = 1
+
+#     @classmethod
+#     def choices(cls):
+#         return [(key.value, key.name) for key in cls]
+
+
+class Program(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Section(models.Model):
+    name = models.CharField(max_length=100)
+    program = models.ForeignKey(Program,
+                                related_name="sections",
+                                on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Activity(models.Model):
+    name = models.CharField(max_length=100)
+    section = models.ForeignKey(Section,
+                                related_name="activities",
+                                on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
