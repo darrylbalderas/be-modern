a247f7b73773a07c7a330fdabde3815028556ceb
darrylbalderas <drrylbalderas@gmail.com>
2020-11-17 10:40:14 -0800

    Remove fields in Object type graphql schema

--- modern_catalog/programs/schema.py
+++ modern_catalog/programs/schema.py
@@ -1,25 +1,21 @@
 import graphene
 from graphene_django import DjangoObjectType
-
 from modern_catalog.programs.models import Program, Section, Activity
 
 
 class ProgramType(DjangoObjectType):
     class Meta:
         model = Program
-        fields = ("id", "name", "sections")
 
 
 class SectionType(DjangoObjectType):
     class Meta:
         model = Section
-        fields = ("id", "name", "activities")
 
 
 class ActivityType(DjangoObjectType):
     class Meta:
         model = Activity
-        fields = ("id", "name")
 
 
 class Query(graphene.ObjectType):
